de9a90f22c59d274ef3c88c37abfe67314fc2b42
Jorden Vande Kop <gamingledger@gmail.com>
2024-02-13 18:58:10 -0600

    first commit

---
+++ unit1Project.js
@@ -0,0 +1,14 @@
+//Name: Jorden Vande Kop
+//Date: 02/11/2024
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+    // Change background color to a certain color
+    document.body.style.backgroundColor = "red";
+  });
+
+const favNumber = prompt("What is your favorite number");
+console.log(favNumber);  
